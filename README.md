6494789dd78e02cd54334c0c2fee7767a458cec1
Nam Nguyen <hnamnguyencse@gmail.com>
2019-09-20 02:26:48 +0700

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Testcase shuffle
+## Description
+This is the minitool to shuffle and re-index testcase of **Principle of Programing Language**'s assignment.
+
+## Usage
+
+ 1. Copy `suffleTestcase.py` to **src** folder
+ 2. Open terminal/console at **src**
+ 3.  Run command
+    > python suffleTestcase.py
+
+This command will find every `*Suite.py` file in `test\`, shuffle it and output to `*Suite_shuffled.py`
+
+## License
+
+Testcase shuffle is licensed under the [MIT license](./LICENSE).
+
+## Author
+__[Nam Nguyen](https://www.facebook.com/namnngguyen)__
---
+++ suffleTestcase.py
@@ -0,0 +1,21 @@
+import os, re, random
+
+files_in_test = os.listdir('./test')
+for i in files_in_test:
+    if i[-8:] == "Suite.py":
+        filename = './test/'+i
+        suite_content = open(filename, 'r').read()
+        header_pos = suite_content.find(':') + 2
+        header = suite_content[:header_pos]
+        body = suite_content[header_pos:]
+        matches = re.findall(r"    def test_.*?,\s*\d{1,3}\)\)", body, re.DOTALL)
+        random.shuffle(matches)
+        matches=matches[:100]
+        try: suite_id = int(matches[0][-5:-4]) * 100
+        except: suite_id = 0
+        for pi, i in enumerate(matches):
+            matches[pi] = re.sub(",\s*\d+\)\)", ", " + str(pi + suite_id) + "))", i)
+        output=header + "\n".join(matches)
+        open(filename[:-3]+"_shuffled.py",'w').write(output)
+
+print("Shuffle done")
